d97de6d8c92ce7ed18edfeb0857871dacb4b5b10
vishnu-new-web-student <164169296+vishnu-new-web-student@users.noreply.github.com>
2024-03-26 12:21:02 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,14 @@ import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host: true,
+    port: 80,
+  },
   
   plugins: [react()],
   define:{
     'process.env.VITE' :JSON.stringify(process.env.VITE_KEY)
-  }
+  },
   
-})
+});
